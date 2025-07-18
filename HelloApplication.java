b6049336eb4f58bdfb92cb639f1bcfa5116417d5
enzovonkunoschy@gmail.com <enzovonkunoschy@gmail.com>
2025-07-18 15:56:51 -0300

    Primeras vistas

--- HelloApplication.java
+++ HelloApplication.java
@@ -1,16 +1,15 @@
 package com.example.ticket;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     private Stage primaryStage;
@@ -25,9 +24,19 @@ public class HelloApplication extends Application {
 
     public void mostrarPantallaInicio() {
         VBox layout = new VBox();
+
+        Label labUser = new Label("Usuario");
+        TextField texUser = new TextField();
+        Label labPass = new Label("Contraseña");
+
+        TextField texPass = new TextField();
+
         Button btnContinuar = new Button("Ir al panel");
-        btnContinuar.setOnAction(e -> mostrarPanelPrincipal());
-        layout.getChildren().add(btnContinuar);
+        btnContinuar.setOnAction(e -> {
+            System.out.println("mostrarPantallaInicio -> mostrarPanelPrincipal");
+            mostrarPanelPrincipal();
+        });
+        layout.getChildren().addAll(labUser, texUser, labPass, texPass, btnContinuar);
 
 
 
@@ -54,17 +63,47 @@ public class HelloApplication extends Application {
         butAceptar.setOnAction(e -> mostrarPanelPrincipal() );
     };
 
+    public void nuevoTicket(){
+
+        Label      labOficina = new Label("Oficina");
+        TextField  texOficina = new TextField();
+        Button butNuevoTicket = new Button("Aceptar");
+
+        VBox layNuevoTicket = new VBox();
+        layNuevoTicket.getChildren().addAll(labOficina, texOficina, butNuevoTicket);
+
+        Scene sceNuevoTicket = new Scene(layNuevoTicket, 400,300);
+        primaryStage.setScene(sceNuevoTicket);
+        primaryStage.setTitle("Nuevo Ticket");
+        primaryStage.show();
+
+        butNuevoTicket.setOnAction(e -> {
+
+            mostrarPanelPrincipal();
+        } );
+    };
+
     public void mostrarPanelPrincipal() {
-        BorderPane layout = new BorderPane();
-        layout.setCenter(new Label("Bienvenido al panel principal"));
+        Pane layout = new Pane();
+
         Button btnSalir = new Button("Cerrar");
+        btnSalir.setLayoutX(10);
+        btnSalir.setLayoutY(10);
+
         btnSalir.setOnAction(e -> primaryStage.close());
-        layout.setBottom(btnSalir);
 
         Button btnRegistrarIngreso = new Button("Registrar Ingreso");
-        //layout.getChildren().add(btnRegistrarIngreso);
-        layout.setBottom(btnRegistrarIngreso);
+        btnRegistrarIngreso.setLayoutX(10);
+        btnRegistrarIngreso.setLayoutY(50);
+
         btnRegistrarIngreso.setOnAction(e -> registrarIngreso());
+
+        Button btnNuevoTicket = new Button("Nuevo Ticket");
+        btnNuevoTicket.setLayoutX(10);
+        btnNuevoTicket.setLayoutY(10);
+        btnNuevoTicket.setOnAction(e-> nuevoTicket());
+
+        layout.getChildren().addAll(btnSalir, btnRegistrarIngreso, btnNuevoTicket);
 
         Scene escena = new Scene(layout, 500, 400);
         primaryStage.setScene(escena);
