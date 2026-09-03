f0def01e9764bd98dae3bcee35641a4c8bc0e65a
RenzoRomo-dev <pedrorenzor@gmail.com>
2026-09-03 17:47:29 -0300

    salto verificacion login

--- v_Login.java
+++ v_Login.java
@@ -43,7 +43,7 @@ public class v_Login {
             System.out.println("button pushed...");
 
 
-           Usuario validado = seguridad.validar(this.txtUser.getText(),this.txtPass.getText());
+           Usuario validado = seguridad.validar("Bob Esponja","4321");
             if( validado != null  ){
 
                 v_Menu.getInstancia(stage, validado);
